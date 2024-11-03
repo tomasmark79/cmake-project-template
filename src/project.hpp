03464ca27ffc229e48b5207f49af363bc186c7a5
tomasmark79 <tomas@digitalspace.name>
2024-11-03 09:45:58 +0100

    removed module division

--- src/project.hpp
+++ src/project.hpp
@@ -3,7 +3,4 @@
 
 constexpr const char *HEADER =
     "\nCMake Project Template 2024 Tomas Mark\n\n";
-constexpr const char *USAGE =
-    "Usage:\n\t <numerator> <denominator>\n\nDescription:\n\tComputes the results of a fractional division,\n\tand reports both the result and the remainder.\n";
-
 #endif // __PROJECT_H__
